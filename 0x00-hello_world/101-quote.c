8d2c001b39a60a88624e009c7d0bd6ef6b10f372
kale-b-elak <kalebelak3537@gmail.com>
2022-10-13 11:31:52 -0700

    task number 1 to 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main block
- * Return: 1
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
