e659ecc019e2a14c1ac62a7bb804aa5d045905ac
kale-b-elak <kalebelak3537@gmail.com>
2022-10-27 10:36:52 +0300

    tasks 0 to 12

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include "holberton.h"
+
 /**
-* print_number - prints an integer
-*@n:integer to be printed
-*
-*/
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+
 void print_number(int n)
 {
 	unsigned int num = n;
