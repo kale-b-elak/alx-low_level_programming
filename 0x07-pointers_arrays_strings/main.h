cba8c9033e0c74ba7fd64c63e3a4da86f2e95c54
kale-b-elak <kalebelak3537@gmail.com>
2022-10-31 18:05:23 +0300

    tasks 0 to 9

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,23 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
- */
-
 #include <stddef.h>
 #include <stdio.h>
 
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+/**
+ * void prototypes(void)
+ * int prototypes(int)
+ * char prototypes(char)
+ */
+
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+unsigned int _strspn(char *s, char *accept);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif /*MAIN_H */
